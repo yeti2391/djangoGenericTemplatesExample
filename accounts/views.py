62dda18a1c52dfbc4bb5acd7078c781e61de327b
yeti2391 <marco2391@gmail.com>
2025-08-25 13:53:38 -0300

    se agrega opcion de crear usuario en app accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+class SignUp(generic.CreateView):
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy('login')
+    form_class = UserCreationForm
--- djangoGenerics/urls.py
+++ djangoGenerics/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hello.urls')),
     path('blog/', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]

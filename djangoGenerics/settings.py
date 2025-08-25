47379a76228b88808d2f6e9839aa0dea7d09cb13
yeti2391 <marco2391@gmail.com>
2025-08-25 15:24:56 -0300

    la sustitucion de plantillas relacionados a la autenticacion y reseteo de password no estan rendereizandose... probablemente por documentacion vieja

--- djangoGenerics/settings.py
+++ djangoGenerics/settings.py
@@ -127,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/blog/posts/'
 LOGOUT_REDIRECT_URL = '/blog/posts/'
+
+# FALSE EMAIL SERVER - Solo para pruebas
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"

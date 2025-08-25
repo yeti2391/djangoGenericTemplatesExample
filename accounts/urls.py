edaa2c1f7f94df877762c894df98f4085f77f7d7
yeti2391 <marco2391@gmail.com>
2025-08-25 14:59:34 -0300

    se agrega funcion de cambio de contrasena pero hay que revisar el mensaje de contrasena cambiada correctamente

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns=[
-    path('signup/', views.SignUp.as_view(), name="signup")
-    # EN EL VIDEO USA URL EN LUGAR DE PATH?
+    path('signup/', views.SignUp.as_view(), name="signup"),
+    path('pass_change/', PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change"),
+    path('pass_change/done/', PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"), name="password_change_done"),
 ]

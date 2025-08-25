32366409ca2dae585450724c26a5eeda75343566
yeti2391 <marco2391@gmail.com>
2025-08-25 13:21:23 -0300

    se agregan opciones login y logout en blog

--- djangoGenerics/settings.py
+++ djangoGenerics/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     #mis apps
     'hello',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/blog/posts/'
+LOGOUT_REDIRECT_URL = '/blog/posts/'
--- djangoGenerics/urls.py
+++ djangoGenerics/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hello.urls')),
     path('blog/', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
